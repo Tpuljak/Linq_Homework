35f087ed37835b2fdac48732e59083c98b1505a3
Toma Puljak <toma.puljak@hotmail.com>
2017-08-17 20:46:45 +0200

    Added first warmup

---
+++ Warmup/Warmup/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Warmup
+{
+    class MainClass
+    {
+        public static bool IsPrime (int number) {
+            if (number == 1 || number == 0) return false;
+            for (int i = 2; i <= number / 2.0; i++) {
+                if (number % i == 0) return false;
+            }
+            return true;
+        }
+
+        public static List<int> GetPrimes (int n) {
+            var OneToN = Enumerable.Range(1, n);
+
+            return OneToN.Where(number => IsPrime(number)).ToList();
+        }
+
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            GetPrimes(n).ForEach(number => Console.WriteLine(number));
+        }
+    }
+}
